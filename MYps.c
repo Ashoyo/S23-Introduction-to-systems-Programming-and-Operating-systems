3cff15e4f65345905a3adbed60a3640382e6f9c1
JimmyJiang <tul70511@temple.edu>
2023-02-21 00:21:58 -0500

    getting started

---
+++ MYps.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+int main(int argc, char const *argv[])
+{
+    
+    int processor=0;
+
+    FILE *cpuinfo = fopen("/proc/cpuinfo", "r");
+
+
+// opens cpu info if fopen works 
+
+    if (cpuinfo)
+    {
+        int processor=-1;
+        int cache = -1;
+
+        char source[10000];
+
+
+
+        while ()
+        {
+
+            
+        }
+        
+
+
+    }
+    
+
+
+
+
+}
