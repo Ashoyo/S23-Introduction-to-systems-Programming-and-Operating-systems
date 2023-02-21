3be6f39136500cd34ed56b14de5dece5df1065c5
JimmyJiang <tul70511@temple.edu>
2023-02-21 18:59:36 -0500

    mod my code and change fget to getline

--- MYps.c
+++ MYps.c
@@ -1,25 +1,30 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+
 
 int main(int argc, char const *argv[])
 {
     
     int processor=0;
+    int cache = -1;
+
+    char *source = NULL;   
+    size_t len = 0;
 
     FILE *cpuinfo = fopen("/proc/cpuinfo", "r");
 
+// check for error 
 
-// opens cpu info if fopen works 
+    if (cpuinfo == NULL) {
+        perror("fopen failed");
+        return 1;
+    }
 
-    if (cpuinfo)
-    {
-        int processor=-1;
-        int cache = -1;
-
-        char source[10000];
-
+ 
 
 // loop to print my processor and cache 
-        while (fgets(source, sizeof(source), cpuinfo))
+        while (getline(&source, &len, cpuinfo)!=-1)
         {
             if (sscanf(source, "processor : %d", &processor) == 1) {
                 processor++;
@@ -27,13 +32,18 @@ int main(int argc, char const *argv[])
                 printf("Processor %d cache size: %d KB\n", processor, cache);
             }
         }
+
         fclose(cpuinfo);
-    }
+        free(source);
+    
      printf(" num of processors: %d\n", processor);
      
      return 0;
             
-        }
+}
+        
+
+
         
 
 
