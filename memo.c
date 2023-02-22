7fffd9880047e2e3b6f69a8469d5d8f9e22b887c
JimmyJiang <tul70511@temple.edu>
2023-02-21 19:03:18 -0500

    used my pro cpuinfo code and mod it to work with memoinf

---
+++ memo.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+
+int main(int argc, char const *argv[])
+{
+    
+    int processor=0;
+    int cache = -1;
+
+    char *source = NULL;   
+    size_t len = 0;
+
+    FILE *cpuinfo = fopen("/proc/cpuinfo", "r");
+
+// check for error 
+
+    if (cpuinfo == NULL) {
+        perror("fopen failed");
+        return 1;
+    }
+
+ 
+
+// loop to print my processor and cache 
+        while (getline(&source, &len, cpuinfo)!=-1)
+        {
+            if (sscanf(source, "processor : %d", &processor) == 1) {
+                processor++;
+            } else if (sscanf(source, "cache size : %d", &cache) == 1) {
+                printf("Processor %d cache size: %d KB\n", processor, cache);
+            }
+        }
+
+        fclose(cpuinfo);
+        free(source);
+    
+     printf(" num of processors: %d\n", processor);
+     
+     return 0;
+            
+}
+        
