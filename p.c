797919a6df00e3b8464919023c58f0367a9be8a9
jimmy <jimmyjiang70@gmail.com>
2023-02-28 16:55:11 -0500

    added from the book to test

--- p.c
+++ p.c
@@ -1,12 +1,21 @@
 #include <stdio.h>
-#include <stdlib.h>
+#include <sys/types.h>
+#include <unistd.h>
 
 
+// stole from the book 
 
+int p() {
+    char target[32];
+    int pid;
+    /* Read the target of the symbolic link. */
+    readlink("/proc/self", target, sizeof(target));
+    sscanf(target, "%d", &pid);
+    return (pid_t) pid;
+}
 
-int p(){
-
-
-
-
+int main() {
+    printf("/proc/self reports process id %d\n", (int) p());
+    printf("getpid() reports process id %d\n", (int) getpid());
+    return 0;
 }
