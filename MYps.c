9a602b72044cfd48e419a41a8b95aff35847747a
jimmy <jimmyjiang70@gmail.com>
2023-03-01 22:56:22 -0500

    fix

--- MYps.c
+++ MYps.c
@@ -57,7 +57,7 @@ int main(int argc, char *argv[])
     // Call   functions based on arg 
     if (s_flag && pid >= 0)
     {
-        char get_process_state(pid);
+        char get_process_state(int pid );
     }
 
     return 0;
