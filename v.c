3b232fead09a3df6e4aa53a28a550bdf6d60f482
jimmy <jimmyjiang70@gmail.com>
2023-03-02 20:15:14 -0500

    .

---
+++ v.c
@@ -0,0 +1,71 @@
+// same logic as the other function 
+
+
+
+// copy stime.c to mod 
+
+
+
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+
+// coopy my utime code to mod, honest just the same code but changing the position to 15
+
+ char*  get_virtualmemo(int pid){
+
+
+
+// copy from my s.c file 
+
+    char stat_file[100];
+    sprintf(stat_file, "/proc/%d/statm", pid); 
+
+    FILE* fp = fopen(stat_file, "r");
+    if (fp == NULL) {
+        perror("error");
+        exit(0);
+
+
+
+    }
+    
+    
+// stores utime there, pointer bc char only store an single splace
+
+    char*  vmeno = (char*) malloc(100 * sizeof(char));
+
+// set to 14 bc the 14 is the postion in the state file for utime
+
+    int position = 1;
+    
+    
+    
+    for (int i = 1; i <= position; i++) {
+        
+        fscanf(fp, "%s", vmeno);
+}
+
+    fclose(fp);
+
+
+
+    // converts utime string to unsign int 
+    
+// strtoul(utime, NULL, 10);
+
+  return vmeno; 
+
+
+
+
+
+
+
+
+
+
+}
